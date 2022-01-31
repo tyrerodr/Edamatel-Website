3545d5c62761c0fc779a7d6c133d9105d2c77c57
rmzevall <rmzevall@espol.edu.ec>
2022-01-31 13:21:23 -0500

    prueba login con api fake

--- src/app/apps/shared/banner/banner.component.ts
+++ src/app/apps/shared/banner/banner.component.ts
@@ -2,6 +2,7 @@ import { KeyValuePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Directive, HostListener } from '@angular/core';
 import { ElementRef } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-banner',
@@ -10,34 +11,47 @@ import { ElementRef } from '@angular/core';
 })
 export class BannerComponent implements OnInit {
 
-  constructor() {
-
-   }
+  constructor(private http : HttpClient){
+     
+  }
 
 
   ngOnInit(): void {
-
-
+    
   }
 
   onSubmit_Login(user: string, password: string) {
-    var users = ["user1", "user2", "user3", "user4", "admin"];
-    var passs = ["12345678", "12345678", "12345678", "12345678", "password"];
-    if(user == "" && password == ""){
-      alert("Complete todos los campos");
-    }else{
-      for(var i = 0; i < users.length ; i++){
-        if(users[i]==user){
-          if(passs[i] != password){
-            alert("Contrasena incorrecta");
-            break;
+    this.http.get('https://run.mocky.io/v3/42ac4610-1565-4691-9fac-f4eae4cf32e8')
+    .subscribe(Response => {
+ 
+      // If response comes hideloader() function is called
+      // to hide that loader
+       
+          var li=Object.entries(Response)
+          var clientes= li[0][1];
+      
+          if(user == "" && password == ""){
+            alert("Complete todos los campos");
           }else{
-            alert("Seion iniciada");
-            break;
+            for(let i in clientes){
+              
+              console.log(clientes[i]["name"]);
+              if(clientes[i]["name"]==user){
+                if(clientes[i]["pass"] != password){
+                  alert("Contrasena incorrecta");
+                  
+                  break;
+                }else{
+                  alert("Seion iniciada");
+                  break;
+                }
+              }
+            }
           }
-        }
-      }
-    }
+    });
+
+
+
   }
 
 
